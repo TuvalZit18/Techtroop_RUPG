e0dcc86bc99c8d552520e32a8261be0e889cc49a
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-19 00:46:32 +0300

    View || Render.js || Add renderProfileCard method

--- view/render.js
+++ view/render.js
@@ -35,3 +35,14 @@ export const renderQuote = (quote) => {
 export const renderAbout = (about) => {
   document.querySelector(".aboutMe-card-footer-text").textContent = about;
 };
+
+export const renderProfileCard = (user) => {
+  document.querySelector(".profile-hero-card-avatar").src = user.picture.large;
+
+  document.querySelector(".profile-hero-card-username").textContent =
+    `${user.firstName} ${user.lastName}`;
+
+  document.querySelector(
+    ".profile-hero-card-location-container span:last-child",
+  ).textContent = `${user.city}, ${user.state}`;
+};
