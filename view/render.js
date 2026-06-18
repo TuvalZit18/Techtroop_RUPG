424e6c5271cc60b3e0f4d84b82f8ec834dbd22c5
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-19 00:39:18 +0300

    View || Render.js || Add renderPokemonCard  method

--- view/render.js
+++ view/render.js
@@ -10,3 +10,20 @@ export const renderMainUser = (user) => {
     ".profile-hero-card-location-container span:last-child",
   ).textContent = `${user.city}, ${user.state}`;
 };
+
+export const renderPokemonCard = (pokemon) => {
+  document.querySelector(".pokemon-level").textContent = `LV.${pokemon.level}`;
+
+  document.querySelector(".pokemon-name").textContent = pokemon.name;
+
+  document.querySelector(".pokemon-type").textContent = Array.isArray(
+    pokemon.type,
+  )
+    ? pokemon.type[0]
+    : pokemon.type;
+
+  document.querySelector(".pokemon-description").textContent =
+    pokemon.description;
+
+  document.querySelector(".pokemon-image").src = pokemon.image;
+};
